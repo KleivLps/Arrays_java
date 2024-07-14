8225aed8159316c6e4c1e792965ddf601bd7ceee
kleiver <kleiver1604@gmail.com>
2024-07-13 22:58:53 -0500

    Ejercicios introductorios para entender como son los arrays en Java.

---
+++ src/Array1.java
@@ -0,0 +1,13 @@
+public class Array1 {
+    public static void main(String[] args) {
+
+        int numero = 6;
+        int[] array1 = {1,numero,2,3}; //array1 es un objeto de la clase int[]
+        int[] array2 = new int[4]; //array2 es un objeto de la clase int[]
+
+        //Al usar llaves, incializamos el array con los valores o variables que desams almacenar, separado por comas.
+
+        //Al usar el operador "new", especificams el nombre de la clase ( en este caso "int[]") y el tamaño del array, ya que los arrays tienen una longitud fija. Tanto "arrayq" como "arrau2" tienen un tamaño de 4 eleemntos. en el caso del "array1", el tamaño se determina automaticamente segun la cantidad de elementos especificados entre las llaves.
+    }
+}
+
---
+++ src/Array2.java
@@ -0,0 +1,7 @@
+public class Array2 {
+    public static void main(String[] args) {
+        String[] arr = new String[3];
+        arr [0] = "hola"; //Modifica el primer elemento del arreglo.
+        System.out.println(arr[0]); //Accede al primer elemento del arreglo
+    }
+}
