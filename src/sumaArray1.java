676fc528eda4fae5916fd78ae9da5dd4a03e94cc
kleiver <kleiver1604@gmail.com>
2024-07-13 23:03:58 -0500

    Programa que suma los elementos dentro del array.

---
+++ src/sumaArray1.java
@@ -0,0 +1,11 @@
+public class sumaArray1 {
+    public static void main(String[] args) {
+
+        int[] array1 = {8,10,12};
+
+        int suma = array1[0] + array1[1] + array1[2];
+
+        System.out.print("La suma de los elementos del array es: " +suma);
+
+    }
+}
